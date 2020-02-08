98c46cc74d51a21e0724bcee7b0c25d8b0276720
crayhj <46726622+crayhj@users.noreply.github.com>
2020-02-08 10:37:55 -0500

    getters and setters for Employee and Client

--- src/Employee.java
+++ src/Employee.java
@@ -1,7 +1,14 @@
 import java.util.ArrayList;
 abstract class Employee {
     private ArrayList Clients = new ArrayList();
-    public Employee(){}
+    private String employeeName;
+    private long salesID;
+    private SalesManager Manager;
+    public Employee(String name, long id, SalesManager manager){
+        employeeName=name;
+        salesID=id;
+        Manager=manager;
+    }
     public boolean addClient(Client client){
         Clients.add(client);
     }
