3dcc9b534b5822bd19a145f905ae3224be2fa188
crayhj <46726622+crayhj@users.noreply.github.com>
2020-02-08 11:39:35 -0500

    Sales Associate toString

--- src/SalesAssociate.java
+++ src/SalesAssociate.java
@@ -3,14 +3,19 @@ import java.util.ArrayList;
 public class SalesAssociate extends Employee {
     public String toString(){
         String AssociateString ="Sales Associate: "+ this.employeeName + ". SalesManager: " + this.Manager.employeeName + " " + this.Manager.salesID +". Clients: ";
-        String clients;
+        String clients="";
         if(this.getClients().size>1) {
             int count=0;
-            while (count<this.getClients().size()){
-                getCliets().get(count).toString();
+            while (count<this.getClients().size()) {
+                clients = clients + getCliets().get(count).toString();
                 count++;
+                if (count = this.getClients().size())
+                    clients = clients + ", ";
+                else
+                    clients = clients + ". ";
             }
         }
+        AssociateString=AssociateString+clients+ "Bonus: "+bonus();
     }
     double bonus(){
         return this.getClients().size()*1000.0;
