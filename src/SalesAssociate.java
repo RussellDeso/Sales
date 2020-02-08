ab4ffba2bc70f6e3e1c44b3a5321b21e50a2968b
crayhj <46726622+crayhj@users.noreply.github.com>
2020-02-08 11:15:30 -0500

    changing Manager and associate

--- src/SalesAssociate.java
+++ src/SalesAssociate.java
@@ -1,21 +1,12 @@
 import java.util.ArrayList;
 
 public class SalesAssociate extends Employee {
-    ArrayList<Employee> employees;
-
-    public ArrayList<Employee> getEmployees() {
-        return employees;
-    }
-
-    public void setEmployees(ArrayList<Employee> employees) {
-        this.employees = employees;
-    }
-
     public String toString(){
 
     }
-
-    Double bonus(){
+    double bonus(){
+        double bonus;
+        clientNum=this.getClients().size();
 
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,9 +1,19 @@
 public class SalesManager extends Employee{
+    ArrayList<Employee> employees;
+
+    public ArrayList<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(ArrayList<Employee> employees) {
+        this.employees = employees;
+    }
+
     public String toString(){
 
     }
 
-    Double bonus(){
+    double bonus(){
 
     }
 }
